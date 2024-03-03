56550ff4f4b9980b2969dca7e1733cecd004c1b1
Asquared <aaronedem17@gmail.com>
2024-03-03 01:34:22 +0100

    List

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // Core Requirements
+
+        // Part 1: Compute the sum
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Part 2: Compute the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Part 3: Find the max
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+
+        // Stretch Challenge
+
+        // Find the smallest positive number
+        int smallestPositive = numbers.Where(x => x > 0).DefaultIfEmpty(0).Min();
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+        // Sort the list
+        List<int> sortedList = numbers.OrderBy(x => x).ToList();
+
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in sortedList)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
