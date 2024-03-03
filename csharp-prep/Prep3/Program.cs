4bcebf4733492f451a69f70abec5f4a879a6a919
Asquared <aaronedem17@gmail.com>
2024-03-03 01:28:15 +0100

    loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess;
+            int attempts = 0;
+
+            Console.WriteLine("Welcome to the Guess My Number game!");
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+            } while (guess != magicNumber);
+
+            Console.WriteLine($"It took you {attempts} attempts to guess the number.");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgainResponse = Console.ReadLine().ToLower();
+
+            if (playAgainResponse != "yes")
+            {
+                playAgain = false;
+            }
+
+            Console.WriteLine();
+        }
     }
 }
